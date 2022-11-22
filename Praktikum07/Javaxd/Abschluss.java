91a6730cb55b7299cdb2f9b6b9cf76837194aeeb
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2022-11-22 22:18:21 +0100

    Blatt7

---
+++ Praktikum07/Javaxd/Abschluss.java
@@ -0,0 +1,13 @@
+public class Abschluss {
+
+	public int ECTSKolloquium = 0;
+	public int ECTSBachelor = 0;
+	public Note[] NotenBachelor;
+	public Note[] NotenKolloquium;
+
+	public float GetNotendurchschnitt() {
+		// TODO - implement Abschluss.GetNotendurchschnitt
+		throw new UnsupportedOperationException();
+	}
+
+}
