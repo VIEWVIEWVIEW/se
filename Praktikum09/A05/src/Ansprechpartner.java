b943ceef2256930b685d26f235aa80e9bce8c4fd
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2022-12-12 14:02:13 +0100

    Null check

--- Praktikum09/A05/src/Ansprechpartner.java
+++ Praktikum09/A05/src/Ansprechpartner.java
@@ -3,4 +3,11 @@ public class Ansprechpartner {
 	public String Nachname;
 	public String Email;
 	public Organisation ZugehoerigeOrganisation;
+
+	Ansprechpartner(Organisation zugehoerigeOrganisation) {
+		if (zugehoerigeOrganisation == null) {
+			throw new IllegalArgumentException("zugehoerigeOrganisation muss gegeben sein");
+		}
+		this.ZugehoerigeOrganisation = zugehoerigeOrganisation;
+	}
 }
